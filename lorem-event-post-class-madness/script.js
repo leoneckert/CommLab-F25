fe9bade42aa8395ccce66927761b2575e577f655
Leon Eckert <leoneckert@mailbox.org>
2025-11-14 11:42:43 +0800

    js events

--- lorem-event-post-class-madness/script.js
+++ lorem-event-post-class-madness/script.js
@@ -5,15 +5,24 @@ console.log(s);
 function move(eventInfo){
     // informatio about the event
     // which triggered this function:
-    console.log(eventInfo.target);
+    console.log(eventInfo);
 
     let elementThatWasMouseOvered = eventInfo.target;
-    elementThatWasMouseOvered.style.color = "red";
-    let randomX = -50 + Math.random()*100;  // random value between -50 and 50
-    let randomY = -50 + Math.random()*100;
-    //                                          "translate( 50px, 50px)" 
-    elementThatWasMouseOvered.style.transform = "translate(" + randomX + "px, " + randomY + "px)";
+    let topString = elementThatWasMouseOvered.style.top;
+    let top = 0;
+    if(topString != ""){
+        top = Number(topString.substring(0, topString.length - 2));
+    }
+    console.log(top)
 
+    // elementThatWasMouseOvered.style.color = "red";
+    let randomX = -50 + Math.random()*100;  // random value between -50 and 50
+    let randomY = top + Math.random()*-100;
+    console.log(randomY)
+    //                                          "translate( 50px, 50px)" 
+    // elementThatWasMouseOvered.style.transform = "translate(" + randomX + "px, " + randomY + "px)";
+    elementThatWasMouseOvered.style.left = randomX + "px";
+    elementThatWasMouseOvered.style.top = randomY + "px";
 
 }
 
@@ -32,7 +41,3 @@ allSpans.forEach(  addMover  );
 
 
 
-
-function leonAdds2(num){
-    console.log(num + 2);
-}
